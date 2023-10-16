0d4d1c5dff81fbc47886356ab93545a3feccbc65
jenny <rejennylo@gmail.com>
2023-10-16 10:47:11 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<HomePage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

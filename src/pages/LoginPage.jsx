cd99c3249531be9b97f69d0aa6d3a1f7b155ba9b
jenny <rejennylo@gmail.com>
2023-10-19 16:52:30 +0800

    feat: Add auth input props and router links

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,8 +6,12 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <AuthContainer>
       <div>
@@ -16,11 +20,22 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="帳號"
+          value={username}
+          placeholder="請輸入帳號"
+          onChange={(usernameInputValue) => setUsername(usernameInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="密碼"
+          value={password}
+          placeholder="請輸入密碼"
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
       </AuthInputContainer>
       <AuthButton>登入</AuthButton>
       <AuthLinkText>註冊</AuthLinkText>
